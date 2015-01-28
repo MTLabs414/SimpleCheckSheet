0baafc46acdd669a5a74d4d5f113305fdcedb604
gonta616 <k-gonjo@cec-ltd.co.jp>
2015-01-29 07:58:21 +0900

    localStrage

--- edit/js/edit.js
+++ edit/js/edit.js
@@ -1,22 +1,23 @@
+/* Write JavaScript here */
 $(function () {
 
   // JSON形式のデータ
   var data =
   {
-    koutei:"",
-    pjStart:"",
-    pjEnd:"",
-    gamenNum:0,
-    chohyoNum:0,
-    bucchiNum:0,
-    kadouNum:0,
-    orderUmu:"",
-    kadouPTRUmu:0,
-    shoudanUmu:"",
-    mitsumoriUmu:"",
-    ssproUmu:"",
-    gyosekiKisaiUmu:"",
-    jissekiUmu:"",
+    koutei:"",                  // 現在の主工程
+    pjStart:"",                 // 開始日
+    pjEnd:"",                   // 終了日
+    gamenNum:0,                 // 計画画面数
+    chohyoNum:0,                // 計画帳票数
+    bucchiNum:0,                // 計画バッヂ数
+    kadouNum:0,                 // 稼働人数
+    orderUmu:"",                // オーダー有無
+    kadouPTRUmu:0,              // 稼働PTR有無
+    shoudanUmu:"",              // 商談有無
+    mitsumoriUmu:"",            // 見積もり有無
+    ssproUmu:"",                // SSPRO有無
+    gyosekiKisaiUmu:"",         // 業績記載有無
+    jissekiUmu:"",              // 実績有無
     excelKisaiUmu:"",
     syuseiKakuninUmu:"",
     mgRenrakuUmu:"",
@@ -26,12 +27,18 @@ $(function () {
     qcdSakuseiUmu:"",           // QCD月例報告書作成有無
     kanriKousu:0,               // 現段階管理工数
     nowZangyouAvg:0,            // 当月の平均残業見込み
-    nextZangyouAvg:0,           // 翌月の平均残業見込み
+    nextZangyouAvg:0            // 翌月の平均残業見込み
+  };
 
-
-  }
-
+  // HTMLから値取り出し
 　data.koutei = $('#kotei').val();
+  data.pjStart = $('#dtPjStartDate').val();
+  data.pjEnd = $('#pjEnd').val();
+  data.gamenNum = $('#gamenNum').val();
+  data.chohyoNum = $('#chohyoNum').val();
+  data.bucchiNum = $('#bucchiNum').val();
+
+
 
   // バリデーション
   $("#registrationForm").validate({
@@ -68,13 +75,21 @@ $(function () {
           }
       }
   });
-  //Validation成功時の遷移先
-  $("input.validate").click(function() {
-      if($("#registrationForm").valid() == true){
-          //location.href = "confirm.html"
-      }
-      return false;
+
+  $('#btn-submit').on('click',function(){
+    //Validation成功時の遷移先
+    $("input.validate").click(function() {
+        if($("#registrationForm").valid() == true){
+
+          localStorage.setItem('input', data);
+
+          location.href =　'../../check/index.html';
+            //location.href = "confirm.html"
+        }
+        return false;
+    });
   });
+
 
   $('#radioBtn a').on('click', function(){
     var sel = $(this).data('title');
